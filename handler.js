f45a8ee1e3428f95dbe06d500f054cb8a2698ec7
test <test@vpn-client-172-16-11-6.rutgers.edu>
2018-08-05 12:59:30 -0400

    REST DB

--- handler.js
+++ handler.js
@@ -1,64 +1,124 @@
 'use strict';
 
-module.exports.tasks_get = (event, context, callback) => {
+const crypto = require('crypto');
+const AWS = require('aws-sdk');
+const client = new AWS.DynamoDB.DocumentClient();
+
+module.exports.tasks_get = async (event, context, callback) => {
   var request = {};
+  var response = {};
+
   if (event.body != null) request = JSON.parse(event.body);
+  if (event.pathParameters && event.pathParameters.id) {
+    response = await client.get({
+      TableName: 'aws-sls-todo-dev',
+      Key: {
+        'uid': event["requestContext"]["authorizer"]["claims"]["cognito:username"],
+        'id': event.pathParameters.id
+      }
+    }).promise();
+  } else {
+    response = await client.query({
+      TableName: 'aws-sls-todo-dev',
+      KeyConditionExpression: 'uid = :uid',
+      ExpressionAttributeValues: {
+        ':uid': event["requestContext"]["authorizer"]["claims"]["cognito:username"]
+      }    
+    }).promise();
+  };
 
-  if (request.pool_id != null
-    && request.client_id != null
-  ) {
-  }
   callback(null, {
     statusCode: 200,
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*"
     },
-    body: JSON.stringify({
-      id: 123,
-      name: 'laundry'
-    })
-  });  
-};
-
-module.exports.tasks_post = (event, context, callback) => {
-  callback(null, {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
-    },
-    body: JSON.stringify({
-      id: 123,
-      name: 'laundry'
-    })
+    body: JSON.stringify(response)
   });    
 };
 
-module.exports.tasks_put = (event, context, callback) => {
+module.exports.tasks_post = async (event, context, callback) => {
+  var request = {};
+  var response = {};
+
+  if (event.body != null) request = JSON.parse(event.body);
+  if (request.name != null) {
+    response = await client.put({
+      TableName: 'aws-sls-todo-dev',
+      Item: {
+          'uid': event["requestContext"]["authorizer"]["claims"]["cognito:username"],
+          'id': crypto.randomBytes(16).toString("hex"),
+          'name': request.name,
+          'status': 'pending'
+      },
+      ReturnValues: 'ALL_OLD'
+    }).promise();
+  };
+
   callback(null, {
     statusCode: 200,
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*"
     },
-    body: JSON.stringify({
-      id: 123,
-      name: 'laundry'
-    })
+    body: JSON.stringify(response)
   });    
 };
 
-module.exports.tasks_delete = (event, context, callback) => {
+module.exports.tasks_put = async (event, context, callback) => {
+  var request = {};
+  var response = {};
+
+  if (event.body != null) request = JSON.parse(event.body);
+  if (event.pathParameters && event.pathParameters.id 
+    && request.status) {
+    response = await client.update({
+      TableName: 'aws-sls-todo-dev',
+      Key: {
+        'uid': event["requestContext"]["authorizer"]["claims"]["cognito:username"],
+        'id': event.pathParameters.id
+      },
+      UpdateExpression: "SET #status = :status",
+      ExpressionAttributeNames: {
+        '#status': 'status'
+      },
+      ExpressionAttributeValues: {
+        ':status': request.status
+      },
+      ReturnValues: 'ALL_NEW'
+    }).promise();
+  };
+
   callback(null, {
     statusCode: 200,
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*"
     },
-    body: JSON.stringify({
-      id: 123,
-      name: 'laundry'
-    })
+    body: JSON.stringify(response)
+  });    
+};
+
+module.exports.tasks_delete = async (event, context, callback) => {
+  var response = {};
+
+  if (event.pathParameters && event.pathParameters.id) {
+    response = await client.delete({
+      TableName: 'aws-sls-todo-dev',
+      Key: {
+        'uid': event["requestContext"]["authorizer"]["claims"]["cognito:username"],
+        'id': event.pathParameters.id
+      },
+      ReturnValues: 'ALL_OLD'
+    }).promise();
+  };
+
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify(response)
   });    
 }
