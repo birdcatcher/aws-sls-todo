37556f0de9af88d6722d12690a306048e1d6b898
test <test@vpn-client-172-16-10-3.rutgers.edu>
2018-08-05 23:52:47 -0400

    fix filename

--- url.sh
+++ url.sh
@@ -1,3 +1,3 @@
 echo https://s.codepen.io/webdesign/debug/ejjEJG?
-node options.js
+node config.js
 sls info -v | grep 'ServiceEndpoint' | sed 's/ServiceEndpoint: https:\/\//serviceEndPoint=/' | sed 's/\(.*\)\.execute.*$/\1/'
